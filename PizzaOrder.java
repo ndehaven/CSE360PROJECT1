7590219e5ab5d9f224dade45a2a61346f615654e
AidanMcClean <aidanmccleanxd@gmail.com>
2022-10-23 15:54:43 -0700

    pizzaorder fixing up
    pizza order fix up

--- PizzaOrder.java
+++ PizzaOrder.java
@@ -2,32 +2,36 @@ import java.util.*;
 
 public class PizzaOrder {
 
-public PizzaOrder() {
+    //instance variables
+    int pizzaType; //0: pepperoni, 1: veggie, 2: cheese
+    boolean mushroom;
+    boolean onion;
+    boolean olives;
+    boolean extraCheese;
+    String pickupTime;
+    boolean acceptedStatus;
 
-    int pizzaType = 0; //0: pepperoni, 1: veggie, 2: cheese
-    boolean mushroom = false;
-    boolean onion = false;
-    boolean olives = false;
-    boolean extraCheese = false;
-    String pickupTime = "0:00";
-    boolean acceptedStatus = false;
-}
+    public PizzaOrder() {
 
-public PizzaOrder(int Type, boolean mushroomi, boolean onioni, boolean olivesi, boolean extraCheesei, String pickupTimei, boolean acceptedStatusi) {
+        int pizzaType = 0; //0: pepperoni, 1: veggie, 2: cheese
+        boolean mushroom = false;
+        boolean onion = false;
+        boolean olives = false;
+        boolean extraCheese = false;
+        String pickupTime = "0:00";
+        boolean acceptedStatus = false;
+    }
 
-    int pizzaType = Type; //0: pepperoni, 1: veggie, 2: cheese
-    boolean mushroom = mushroomi;
-    boolean onion = onioni;
-    boolean olives = olivesi;
-    boolean extraCheese = extraCheesei;
-    String pickupTime = pickupTimei;
-    boolean acceptedStatus = acceptedStatusi;
+    public PizzaOrder(int Type, boolean mushroomi, boolean onioni, boolean olivesi, boolean extraCheesei, String pickupTimei, boolean acceptedStatusi) {
 
-}
+        int pizzaType = Type; //0: pepperoni, 1: veggie, 2: cheese
+        boolean mushroom = mushroomi;
+        boolean onion = onioni;
+        boolean olives = olivesi;
+        boolean extraCheese = extraCheesei;
+        String pickupTime = pickupTimei;
+        boolean acceptedStatus = acceptedStatusi;
+    }
 
-//main to test code
-public static void main(String[] args) {  
-    
-    System.out.println(PizzaOrder.CheckId("testcase"));
-}
+
 }
