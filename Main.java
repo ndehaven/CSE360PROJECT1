8727920747bb65994901a1a595573ff83aa3f45d
aaorti15 <aaorti15@asu.edu>
2022-10-23 16:39:41 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 import java.util.*; 
-
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.StackPane;
+import java.util.ArrayList;
 
 
 /*public static boolean CheckId() {
