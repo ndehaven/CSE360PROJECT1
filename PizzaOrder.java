3f995bae25585a24b0362f56eac8ba5a1c2b9904
AidanMcClean <aidanmccleanxd@gmail.com>
2022-10-23 15:49:12 -0700

    PizzaOrder Constructors
    prototype for PizzaOrder constructors complete

--- PizzaOrder.java
+++ PizzaOrder.java
@@ -2,7 +2,26 @@ import java.util.*;
 
 public class PizzaOrder {
 
-public static void SendOrder(PizzaOrder customerOrder) {
+public PizzaOrder() {
+
+    int pizzaType = 0; //0: pepperoni, 1: veggie, 2: cheese
+    boolean mushroom = false;
+    boolean onion = false;
+    boolean olives = false;
+    boolean extraCheese = false;
+    String pickupTime = "0:00";
+    boolean acceptedStatus = false;
+}
+
+public PizzaOrder(int Type, boolean mushroomi, boolean onioni, boolean olivesi, boolean extraCheesei, String pickupTimei, boolean acceptedStatusi) {
+
+    int pizzaType = Type; //0: pepperoni, 1: veggie, 2: cheese
+    boolean mushroom = mushroomi;
+    boolean onion = onioni;
+    boolean olives = olivesi;
+    boolean extraCheese = extraCheesei;
+    String pickupTime = pickupTimei;
+    boolean acceptedStatus = acceptedStatusi;
 
 }
 
