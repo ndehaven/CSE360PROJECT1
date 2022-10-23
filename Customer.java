71a489aeb556b1326dd9561e48c5102814b13ac3
aaorti15 <aaorti15@asu.edu>
2022-10-23 15:02:55 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,26 @@
 import java.util.*; 
+
+public class Customer {
+	//Instance Variables
+	private String asuID;
+	private String asuEmail;
+	private PizzaOrder pizza;
+
+	public Customer(String ID,  String email, PizzaOrder order) {
+		asuID = ID;
+		asuEmail = email;
+		pizza = new PizzaOrder(order)
+	}
+
+	public String getID(){
+		return asuID;
+	}
+
+	public String getEmail() {
+		return asuEmail;
+	}
+
+	public PizzaOrder getOrder() {
+		return pizza;
+	}
+}
