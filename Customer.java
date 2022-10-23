66b68e9a56b9d7985ebc64ba42eeebce9ea978a8
aaorti15 <aaorti15@asu.edu>
2022-10-23 15:38:57 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -25,11 +25,12 @@ public class Customer {
 		return new PizzaOrder(pizza);
 	}
 
-		public static boolean CheckId(String asuID) {
-			if (asuID instanceof String) { //checking if ID is a string or not for now
+	public static boolean CheckId() {
+		if (asuID instanceof String) { //checking if ID is a string or not for now
 				return true;
-			} else {
+		} 
+		else {
 				return false;
-				}
+			}
 		}
 }
