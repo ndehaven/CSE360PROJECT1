b1e42c57c714839f5596dd0c14fd4a6298bd00d8
AidanMcClean <aidanmccleanxd@gmail.com>
2022-10-23 17:54:25 -0700

    Trying to implement stack
    added a few things to almost everything

--- project1CSE360/src/ChefDisplay.java
+++ project1CSE360/src/ChefDisplay.java
@@ -1,3 +1,22 @@
-public class ChefDisplay {
-    
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class ChefDisplay {  
+
+    @FXML
+    void cookingStatus(ActionEvent event) {
+        PizzaOrder.setStatus("Ready to Cook");
+    }
+
+    @FXML
+    void finishStatus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void readyStatus(ActionEvent event) {
+
+    }
+
 }
--- project1CSE360/src/Main.java
+++ project1CSE360/src/Main.java
@@ -6,8 +6,23 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.StackPane;
 import java.util.ArrayList;
+import java.util.Stack;
+import java.util.*;
+import java.io.*;
+
+public class Main {
 
 
+	public static void main(String[] args) {
+
+		Stack<E> orders = new Stack<E>();
+
+		orders.push(2);
+
+	}
+
+
+}
 /*public static boolean CheckId() {
 	if (asuID instanceof String) { //checking if ID is a string or not for now
 			return true;
