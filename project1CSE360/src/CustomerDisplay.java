891dc02797c8e5e4ed2e870f92ed46cc68993de7
aaorti15 <aaorti15@asu.edu>
2022-10-23 18:45:40 -0700

    Update CustomerDisplay.java

--- project1CSE360/src/CustomerDisplay.java
+++ project1CSE360/src/CustomerDisplay.java
@@ -1,3 +1,77 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
 public class CustomerDisplay {
-    
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Font x11;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Color x21;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+
+    @FXML
+    void CheckOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void asuID(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pickTime(ActionEvent event) {
+
+    }
+
+    @FXML
+    void topExtraCheese(ActionEvent event) {
+
+    }
+
+    @FXML
+    void topMushroom(ActionEvent event) {
+
+    }
+
+    @FXML
+    void topOlives(ActionEvent event) {
+
+    }
+
+    @FXML
+    void topOnion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void typeCheese(ActionEvent event) {
+
+    }
+
+    @FXML
+    void typePepperoni(ActionEvent event) {
+
+    }
+
+    @FXML
+    void typeVeggie(ActionEvent event) {
+
+    }
+
 }
+
